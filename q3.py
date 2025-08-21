a7a0f43320c31be597b247b03a2cb36b0bc52491
Azhu0921 <chaiyee6192@gmail.com>
2025-08-21 20:44:58 +0800

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,11 @@
+def update_dictionary(dct, key, value):
+
+    if key in dct:
+        print(f"Original value for '{key}': {dct[key]}")
+    dct[key] = value
+    return dct
+
+
+# Task 2:
+print(update_dictionary({}, "name", "Alice"))
+print(update_dictionary({"age": 25}, "age", 26))
