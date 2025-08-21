c19f0737d9ba2196eee10208eeea812bea0626ac
Azhu0921 <chaiyee6192@gmail.com>
2025-08-21 20:38:10 +0800

    q2.py

---
+++ q2.py
@@ -0,0 +1,15 @@
+def find_and_replace(lst, find_val, replace_val):
+
+    for i in range(len(lst)):
+
+        if lst[i] == find_val:
+
+          lst[i] = replace_val
+
+    return lst
+
+
+# Task 2
+print(find_and_replace([1, 2, 3, 4, 2, 2], 2, 5))
+
+print(find_and_replace(["apple", "banana", "apple"], "apple", "orange"))
